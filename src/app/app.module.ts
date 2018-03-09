f47c304d67f35c6c0c13548d962089511424e8a3
Omar Mohib <mora_ayad@hotmail.com>
2018-03-09 09:47:53 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
 
+
+const appRoutes: Routes = [
+  { path: 'index', component: IndexComponent },
+  { path: '**', component: IndexComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
