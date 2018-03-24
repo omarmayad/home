dbc8bcba5fb848c4d95770581111b2693d2085d7
Omar Mohib <mora_ayad@hotmail.com>
2018-03-24 23:40:46 +0200

    buggy but working no questions

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -37,6 +37,28 @@ export class IndexComponent implements OnInit {
     this.percentage += (90/this.questionsList.length);
     this.currentQuestion=this.questionsList[this.index];
     this.AllTheSelectedOptions.push($event);
+    if(this.AllTheSelectedOptions[7]){
+      if(this.AllTheSelectedOptions[7].selected == "No" && !this.AllTheSelectedOptions[7].passed){
+        this.AllTheSelectedOptions[7].passed = true;
+        this.index+=2;
+        this.percentage += (90/this.questionsList.length +1);
+        this.currentQuestion=this.questionsList[this.index];
+    
+      }
+    }
+ 
+    if(this.AllTheSelectedOptions[16]){
+      console.log("working",this.currentQuestion);
+      if(this.AllTheSelectedOptions[16].selected == "No" && !this.AllTheSelectedOptions[16].passed){
+       
+        this.AllTheSelectedOptions[16].passed = true;
+        this.index+=2;
+        this.percentage += (90/this.questionsList.length +7);
+        this.currentQuestion=this.questionsList[this.index];
+    
+      }
+    }
+    
     if(this.index <= 22){
       //go to final page
       localStorage.setItem('options', JSON.stringify(this.AllTheSelectedOptions));
